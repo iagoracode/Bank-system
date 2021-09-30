c1ac9223634505562f0268a24a5786422170b450
iagoracode <iagorenator@gmail.com>
2021-09-30 17:54:31 -0300

    Identando

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import {Diretor} from "./Funcionarios/Diretor.js"
 import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
 const diretor =  new Diretor("Julio", 10000, 12345678900);
-diretor.cadastrarSenha("1234")
 const gerente =  new Gerente("Carlos",  5000, 12378945601);
-gerente.cadastrarSenha("1234");
-
 const cliente = new Cliente("Fulano", 78945612379, "4567");
+gerente.cadastrarSenha("1234");
+diretor.cadastrarSenha("1234");
+
 const gerenteEstaLogado = SistemaAutenticacao.login(gerente, "1234");
 const diretorEstaLogado = SistemaAutenticacao.login(diretor, "1234");
 const clienteEstaLogado = SistemaAutenticacao.login(cliente, "4567");
